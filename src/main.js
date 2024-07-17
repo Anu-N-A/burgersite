d9736a9eb3a70d29248a6ccbbc9b24b5a2c29a62
Anu-N-A <anuna532002@gmail.om>
2024-07-17 14:50:25 +0530

    first

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById('nav-menu');
-const closeIcon = document.getElementById("nav-close");
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById('nav-menu');
+    const closeIcon = document.getElementById("nav-close");
+    const navLink = document.querySelectorAll(".nav__link");
 
-closeIcon.addEventListener("click",() => {
-    navMenu.classList.add('hidden')
-})
+    navLink.forEach( link => 
+        link.addEventListener("click",() => {
+            navMenu.classList.add('hidden')
+        })
+    )
+    closeIcon.addEventListener("click",() => {
+        navMenu.classList.add('hidden')
+    })
 
-hamburger.addEventListener("click",() => {
-    navMenu.classList.remove('hidden')
-})
+    hamburger.addEventListener("click",() => {
+        navMenu.classList.remove('hidden')
+    })
