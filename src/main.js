976313bc6e6de74db01a9f898ac2fce73d18da64
Anu-N-A <anuna532002@gmail.om>
2024-07-18 14:53:36 +0530

    sixth commit

--- src/main.js
+++ src/main.js
@@ -15,3 +15,45 @@
     hamburger.addEventListener("click",() => {
         navMenu.classList.remove('hidden')
     })
+
+    const tabs = document.querySelectorAll(".tabs_wrap ul li");
+    const all = document.querySelectorAll('.item-wrap');
+    const food = document.querySelectorAll('.food');
+    const snack = document.querySelectorAll('.snack');
+    const beverage = document.querySelectorAll('.beverage')
+
+    tabs.forEach(tab => {
+        tabs.addEventListener("click", () => {
+            tabs.forEach(tab => {
+                tab.classList.add('active')
+            })
+
+            tab.classList.add('active')
+
+            const tabval = tab.getAttribute('data-tabs')
+            
+            all.forEach(item => {
+                item.style.display = 'none'
+            })
+
+            if (tabval == 'food'){
+                foods.forEach(item => {
+                    item.style.display = 'block'
+                })
+            }
+            else if (tabval == 'snack'){
+                snack.forEach(item => {
+                    item.style.display = 'block'
+                })
+            }
+            else if (tabval == 'beverage'){
+                beverage.forEach(item => {
+                    item.style.display = 'block'
+                })
+            }
+            else {
+                all.forEach(item => {
+                    item.style.display = 'block'
+                })
+        })
+    })
